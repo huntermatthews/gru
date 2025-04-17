e149f070731298ccbd9a8b94c1856478c3060012
Hunter Matthews <hunter.matthews@nih.gov>
2025-04-17 18:55:27 -0400

    Get rid of dependancies and add a tracing level.

--- debug.fish
+++ debug.fish
@@ -1,7 +1,9 @@
 ## -*- mode: fish -*- ##
 
-# This is a global variable that is used to control the debug state.
-set -g _debug false
+# This file should have NO dependencies on other files.
+
+# This is a global variable that is used to control the debug state. off|debug|trace
+set -g _debug off
 
 function debug_state
     # This function is used to set the debug state.
@@ -12,20 +14,20 @@ function debug_state
 
     # single argument is our subcommand
     switch $argv[1]
-        case on
-            set _debug true
-        case off
-            set _debug false
+        case debug trace off
+            set _debug $argv[1]
         case status
-            echo "DEBUG: debug state is $_debug"
+            echo $_debug
+        case print_status
+            echo "DEBUG: status is $_debug"
         case '*'
-            echo "ERROR: Unknown subcmd $argv[1]"
-            echo "Usage: debug_state on|off|status"
+            echo "ERROR: Unknown arg $argv[1]"
+            echo "Usage: debug_state off|debug|trace|status|print_status"
     end
 end
 
 function debug
-    if is_true $_debug
+    if test "$_debug" != off
         echo "DEBUG: $argv" 1>&2
     end
 end
@@ -39,8 +41,8 @@ function debug_var -S
         echo "ERROR: You are a moron. You can't debug_var argv itself..." 1>&2
         exit 11
     end
-    if is_true $_debug
-        echo "DEBUG: variable $argv[2] == '$$argv[2]'" 1>&2
+    if test "$_debug" != off
+        echo "DEBUG: variable $argv[1] == '$$argv[1]'" 1>&2
     end
 end
 
@@ -48,15 +50,22 @@ end
 # This function will print out the variable name, and then each index of the variable on a new line.
 function debug_var_list -S
     # The -S is magic that allows this debug function to peer into other functions private variables...
-    if is_true $_debug
+    if test "$_debug" != off
         set -l count (count $$argv[1])
         if test $count -eq 0
-            echo "DEBUG: variable $argv[1] of length $count == '$$argv[1]'" 1>&2
+            echo "DEBUG: variable `$argv[1]` of length $count == '$$argv[1]'" 1>&2
         else
-            echo "DEBUG: variable $argv[1] of length $count:" 1>&2
+            echo "DEBUG: variable `$argv[1]` of length $count:" 1>&2
             for i in (seq (count $$argv[1]))
                 echo "     debug: index: $i, Value: $$argv[1][$i]" 1>&2
             end
         end
+    end
+end
+
+# FIXME: yeah, there's a macos program named "trace" -- I don't care at the moment.
+function trace
+    if test "$_debug" = trace
+        echo "TRACE: $argv" 1>&2
     end
 end
