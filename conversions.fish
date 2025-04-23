5f1f3d4184e764697f2eaad8b2fbf29ca70bb7ec
Hunter Matthews <hunter@unix.haus>
2025-04-23 01:01:19 -0400

    Add si_to_bytes function and some tests

--- conversions.fish
+++ conversions.fish
@@ -14,8 +14,39 @@ function bytes_to_si -a size
     end
 
     if test $unit = B
-        printf "%.0f    %s\n" $size $unit
+        printf "%.0f %s\n" $size $unit
     else
         printf "%.02f %s\n" $size $unit
     end
 end
+
+
+# for https://stackoverflow.com/questions/4399475/unformat-disk-size-strings
+# written by Dennis Williamson 2010-12-09
+# ported (fish) by Hunter Matthews 2025-04-22
+function si_to_bytes -a size 
+
+    set units K M G T P E     # kilo, mega, giga, tera, peta, exa
+    set size (string upper $size)
+    set parts (string match --regex --groups-only '([0-9]*[.]?[0-9]+) *([KMGTPE]?)B?' $size)  # match the number, unit, and B
+    set number $parts[1]                                      # get the number
+    set unit $parts[2]                                        # get the unit
+    debug_var parts
+    debug_var number
+    debug_var unit
+
+    set exponent (contains --index $unit $units) 
+    debug_var exponent
+
+    if test -z "$exponent"
+        set exponent 0
+    # else
+    #     set exponent (math "$exponent - 1") # adjust for 0-based index
+    end
+    debug_var exponent
+
+    printf "%u\n" (math "$number * 1024 ^ $exponent")
+end
+
+
+
